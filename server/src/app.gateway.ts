6eaf6e0d1f988230606722899553b7b6745e3cf1
Michal <vanata.michal@gmail.com>
2022-06-14 00:59:06 +0200

    Socket pushed to version 4

--- server/src/app.gateway.ts
+++ server/src/app.gateway.ts
@@ -31,8 +31,7 @@ export class QuickSearchGateway
   @WebSocketServer() server: Server;
 
   handleConnection(client: Socket, ...args: any[]) {
-    this.searchService.joinQuickQueue(Number(client.id));
-
+    this.searchService.joinQuickQueue(client.id);
     const matchedPlayers = this.searchService.tryMatchPlayersQuickQue();
     if (matchedPlayers !== null) {
       const { roomID } = this.gameService.generateQuickGameRoom();
@@ -47,7 +46,7 @@ export class QuickSearchGateway
   }
 
   handleDisconnect(client: Socket) {
-    this.searchService.leaveQuickQueue(Number(client.id));
+    this.searchService.leaveQuickQueue(client.id);
   }
 }
 
--- server/src/game/services/search.service.ts
+++ server/src/game/services/search.service.ts
@@ -2,26 +2,27 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class SearchService {
-  quickSearchQueue: number[] = [];
+  quickSearchQueue: string[] = [];
 
   printQueue(): void {
     console.log(this.quickSearchQueue);
   }
 
-  joinQuickQueue(socketID: number) {
+  joinQuickQueue(socketID: string) {
+    this.printQueue();
     this.quickSearchQueue.push(socketID);
   }
 
-  leaveQuickQueue(socketID: number) {
+  leaveQuickQueue(socketID: string) {
     if (this.quickSearchQueue.includes(socketID)) {
       const indexOfSocket = this.quickSearchQueue.indexOf(socketID);
       this.quickSearchQueue.splice(indexOfSocket, 1);
     }
   }
 
-  tryMatchPlayersQuickQue(): [number, number] | null {
+  tryMatchPlayersQuickQue(): [string, string] | null {
     if (this.quickSearchQueue.length >= 2) {
-      return this.quickSearchQueue.splice(0, 2) as [number, number];
+      return this.quickSearchQueue.splice(0, 2) as [string, string];
     } else {
       return null;
     }
