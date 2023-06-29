b15a68091a3865c714c863953073ffd8e0754e04
Michal <mvanata@redhat.com>
2023-06-29 17:25:37 +0200

    Removed hash from URL link to profile -> Will scroll back to previous position after coming back to it now

--- client/src/components/TheNavbar.vue
+++ client/src/components/TheNavbar.vue
@@ -151,7 +151,7 @@
                 >
                   <a
                     href="#"
-                    @click.prevent="pushAndCloseDropdown('/profile#Start')"
+                    @click.prevent="pushAndCloseDropdown('/profile')"
                     class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-300 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
                     role="menuitem"
                     >Your Profile</a
