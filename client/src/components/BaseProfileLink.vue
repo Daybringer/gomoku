46f701deeee96b00ecd5150011f43881a5ab890f
Michal <mvanata@redhat.com>
2023-07-17 16:59:41 +0200

    Moved ProfileLink from button onclick redirect to router-link because of accesibility issues

--- client/src/components/BaseProfileLink.vue
+++ client/src/components/BaseProfileLink.vue
@@ -1,24 +1,19 @@
 <template>
-  <div
-    @click="
-      () => {
-        if (isLogged) {
-          router.push(link);
-        }
-      }
-    "
+  <router-link
+    target="_blank"
+    :to="isLogged ? link : ''"
+    :is="isLogged ? 'span' : 'router-link'"
     :class="isLogged ? 'cursor-pointer' : 'cursor-not-allowed'"
     class="flex flex-row place-items-center justify-center overflow-auto bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-500 hover:bg-gray-500 gap-1 rounded-lg py-1 px-4"
   >
     <ProfileIconComponent :profile-icon="profileIcon" />
     <span>{{ username }}</span>
-  </div>
+  </router-link>
 </template>
 <script setup lang="ts">
 import { computed } from "vue";
 import { ProfileIcon } from "@/shared/icons";
 import ProfileIconComponent from "./ProfileIcon.vue";
-import router from "@/router";
 const props = defineProps<{
   isLogged: boolean;
   username: string;
